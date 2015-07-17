2f00b81e3ad0fc996e544d670230ac6628deed14
tuber <btu@thoughtworks.com>
2015-07-17 10:43:06 +0800

    [tuber] create build task

---
+++ gulpfile.babel.js
@@ -0,0 +1,14 @@
+import gulp from 'gulp'
+import gutil from 'gulp-util'
+import loadTasks from 'require-dir'
+import _ from 'lodash'
+
+loadTasks('./tasks')
+//_.set(gutil.env, 'base.src', 'src')
+//_.set(gutil.env, 'base.dist', 'dist')
+_.set(gutil.env, 'tasks.build', {
+    taskQuene: [
+        //'clean'
+    ]
+})
+gulp.task('default', [])
---
+++ tasks/build.js
@@ -0,0 +1,13 @@
+import gulp from 'gulp'
+import _ from 'lodash'
+import gutil from 'gulp-util'
+import runSequence from 'run-sequence'
+
+const TASK_NAME = 'build'
+export default gulp.task(TASK_NAME, (cb)=> {
+    const defaultConfig = {
+        taskQuene: []
+    }
+    const conf = _.merge({}, defaultConfig, _.get(gutil.env, ['tasks', TASK_NAME]))
+    //runSequence.apply(gulp, [].concat(conf.taskQuene).concat(cb))
+})
