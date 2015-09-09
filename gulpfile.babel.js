8f93caa66c1c1a75876fd229afc4c00a2c2f4fa3
tuber <btu@thoughtworks.com>
2015-09-09 15:56:00 +0800

    [tuber] add .editorconfig

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,26 +8,26 @@ loadTasks('./tasks')
 _.set(gutil.env, 'base.src', 'src')
 _.set(gutil.env, 'base.dest', 'dest')
 _.set(gutil.env, 'tasks.build', {
-    taskQuene: [
-        'clean',
-        'copy',
-        'jade',
-        'stylus',
-        'browserify'
-    ]
+  taskQuene: [
+    'clean',
+    'copy',
+    'jade',
+    'stylus',
+    'browserify'
+  ]
 })
 
 if (gutil.env.prod) {
-    process.env.NODE_ENV = 'production'
+  process.env.NODE_ENV = 'production'
 }
 
 if (gutil.env.watch) {
-    watcher.setWatcher()
+  watcher.setWatcher()
 }
 
 gulp.task('dev', ()=> {
-    watcher.setWatcher()
-    gulp.start(['build', 'server'])
+  watcher.setWatcher()
+  gulp.start(['build', 'server'])
 })
 
 gulp.task('default', ['build'])
